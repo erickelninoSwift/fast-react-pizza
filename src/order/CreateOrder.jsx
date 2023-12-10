74d34763969f147c2c650f0cc58f367b262094fb
Erick El nino <erick.tshimbombo@yahoo.com>
2023-12-11 01:35:24 +0200

    update

--- src/order/CreateOrder.jsx
+++ src/order/CreateOrder.jsx
@@ -45,16 +45,18 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input" type="text" name="customer" required />
+          <input className="input grow" type="text" name="customer" required />
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center ">
+        <div className="flex- mb-5 flex gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Phone number</label>
-          <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
-            <input className="input" type="tel" name="phone" required />
+
+          <div className="grow">
+            <input className="input w-full" type="tel" name="phone" required />
           </div>
+
           {formError?.phone && (
-            <p>
+            <p className="mt-2 rounded-md bg-red-200 p-2 text-xs text-red-700">
               {' '}
               Please give us your correct phone number . we might need it to
               contact you{' '}
@@ -63,13 +65,18 @@ function CreateOrder() {
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
-          <label>Address</label>
-          <div>
-            <input className="input" type="text" name="address" required />
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              className="input w-full"
+              type="text"
+              name="address"
+              required
+            />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex flex-row items-center gap-2">
           <input
             className="h-5 w-5 accent-yellow-500 focus:ring"
             type="checkbox"
@@ -78,14 +85,14 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label className="px-[10px]" htmlFor="priority">
+          <label className="font-medium" htmlFor="priority">
             Want to yo give your order priority?
           </label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button isDisabled={isSubmitting}>
+          <Button isDisabled={isSubmitting} type={'primary'}>
             {isSubmitting ? 'Placing order' : 'Order now'}
           </Button>
         </div>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -6,14 +6,14 @@ const Button = ({ children, isDisabled, to, type }) => {
   //   'mt-[10px] inline-block rounded-full bg-yellow-400 px-4 py-3 text-sm font-semibold uppercase tracking-wide text-stone-800 hover:bg-yellow-700 hover:text-stone-100 focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4';
 
   const base =
-    'inline-block rounded-full bg-yellow-400  text-sm font-semibold uppercase text-stone-800 tracking-wide hover:bg-yellow-700 hover:text-stone-100 focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ';
+    'inline-block text-sm rounded-full bg-yellow-400  text-sm font-semibold uppercase text-stone-800 tracking-wide hover:bg-yellow-700 hover:text-stone-100 focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ';
 
   const styles = {
     primary: base + ' px-4 py-3 md:px-6 md:py-4',
     small: base + ' py-2 px-5 text-xs md:py-2.5 md:px-7',
     secondary:
       base +
-      'bg-transparent border-2 border-stone-300  px-4 py-2.5 md:px-6 md:py-3.5 hover:bg-stone-600 hover:text-stone-200 focus:bg-stone-400 focus:ring focus:ring-stone-600 focus:ring-offset-2',
+      'bg-transparent text-sm border-2 border-stone-300  px-4 py-2.5 md:px-6 md:py-3.5 hover:bg-stone-600 hover:text-stone-200 focus:bg-stone-400 focus:ring focus:ring-stone-600 focus:ring-offset-2',
   };
   if (to)
     return (
