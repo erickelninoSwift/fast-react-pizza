f1833f7ac63a2f459e3c476ffa7aea06294eecc7
Erick El nino <erick.tshimbombo@yahoo.com>
2023-12-13 12:22:15 +0200

    update

--- src/cart/cartSlice.js
+++ src/cart/cartSlice.js
@@ -11,9 +11,35 @@ const cartSlice = createSlice({
       // Adding a new object in the cart
       state.cart.push(action.payload);
     },
-    deleteItem(state, action) {},
-    addQuantity(state, action) {},
-    removeQuantity(state, action) {},
-    clearCart(state, action) {},
+    deleteItem(state, action) {
+      //payload will be equal to the id of the item
+      state.cart = state.cart.filter((data) => {
+        return data.pizzaId !== action.payload;
+      });
+    },
+    addQuantity(state, action) {
+      const item = state.cart.find((data) => {
+        return data.pizzaId === action.payload;
+      });
+
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    removeQuantity(state, action) {
+      const item = state.cart.find((data) => {
+        return data.pizzaId === action.payload;
+      });
+
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
   },
 });
+
+export const { addItem, deleteItem, addQuantity, removeQuantity, clearCart } =
+  cartSlice.actions;
+
+export default cartSlice.reducer;
