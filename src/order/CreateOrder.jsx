1019ce4355a003d7147a01bc48472a3bc5e1f2a7
Erick El nino <erick.tshimbombo@yahoo.com>
2023-12-19 01:45:37 +0200

    update

--- src/order/CreateOrder.jsx
+++ src/order/CreateOrder.jsx
@@ -7,6 +7,7 @@ import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
 import EmptyCart from '../cart/EmptyCart';
 import store from '../store/store';
 import { formatCurrency } from '../utils/helpers';
+import { useDispatch } from 'react-redux';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -16,6 +17,7 @@ const isValidPhone = (str) =>
 
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
+  const dispatch = useDispatch();
   const username = useSelector((state) => {
     return state.user.username;
   });
@@ -31,6 +33,7 @@ function CreateOrder() {
   return (
     <div>
       <h2 className="mb-5 mt-5">Ready to order? Let's go!</h2>
+      <button onClick={() => {}}></button>
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
